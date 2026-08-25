1b30739d98e92449218cffb09d32fad9055bf77d
vedantoncloud <vedantmanikmishra@gmail.com>
2026-08-25 22:01:07 +0530

    feat: add real AWS connectivity check

--- backend/services/aws_service.py
+++ backend/services/aws_service.py
@@ -1,7 +1,25 @@
+import boto3
+from botocore.exceptions import BotoCoreError, ClientError
+
+
 class AWSService:
     def get_status(self):
-        return {
-            "provider": "AWS",
-            "status": "connected",
-            "message": "AWS service layer is ready",
-        }
+        try:
+            sts = boto3.client("sts")
+            identity = sts.get_caller_identity()
+
+            return {
+                "provider": "AWS",
+                "status": "connected",
+                "account_id": identity.get("Account"),
+                "arn": identity.get("Arn"),
+                "message": "AWS connection is working",
+            }
+
+        except (BotoCoreError, ClientError) as error:
+            return {
+                "provider": "AWS",
+                "status": "disconnected",
+                "message": "Unable to connect to AWS",
+                "error": str(error),
+            }
