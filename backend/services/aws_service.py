95859fda9894c86822c2bd70e72d211b2046b041
vedantoncloud <vedantmanikmishra@gmail.com>
2026-08-26 19:31:19 +0530

    feat: improve AWS service status

--- backend/services/aws_service.py
+++ backend/services/aws_service.py
@@ -8,18 +8,25 @@ class AWSService:
             sts = boto3.client("sts")
             identity = sts.get_caller_identity()
 
+            s3 = boto3.client("s3")
+            s3.list_buckets()
+
             return {
                 "provider": "AWS",
                 "status": "connected",
                 "account_id": identity.get("Account"),
                 "arn": identity.get("Arn"),
-                "message": "AWS connection is working",
+                "services": {
+                    "sts": "healthy",
+                    "s3": "healthy",
+                },
+                "message": "AWS services are healthy",
             }
 
         except (BotoCoreError, ClientError) as error:
             return {
                 "provider": "AWS",
                 "status": "disconnected",
-                "message": "Unable to connect to AWS",
+                "message": "Unable to connect to AWS services",
                 "error": str(error),
             }
